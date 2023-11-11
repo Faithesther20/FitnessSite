b8b28cf0fb8ab2dbbf61a7ccb1c4d141730152f4
Esther J. Iyege <faithesther20@gmail.com>
2023-11-11 11:31:36 +0100

    fitnessSite started

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import { useState,useEffect } from 'react';
+
+import Navbar from "@/scenes/navbar/index";
+import { SelectedPage } from '@/shared/type';
+
+const [isTopOfPage, setIsTopOfPage] = useState(true)
+   
+
+function App() {
+ const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home)
+ 
+const [isTopOfPage, setIsTopOfPage] = useState(true)
+
+useEffect(() => {
+  const handleScroll =() =>{
+    if(window.scrollY === 0){
+      setIsTopOfPage(true);
+      setSelectedPage(SelectedPage.Home);
+    }
+    if(window.scrollY !== 0 ) setIsTopOfPage(false);
+  }
+  window.addEventListener("scroll", handleScroll);
+  return() => window.removeEventListener('scroll', handleScroll);
+   
+ },[])
+ 
+ return (
+    <div className='app bg-gray-20'>
+      <Navbar
+      isTopOfPage = {isTopOfPage}
+      selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
+    </div>
+  )
+}
+
+export default App
